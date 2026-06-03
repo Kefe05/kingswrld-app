a5d447071c7f75ade5cd982a9410966f6e986711
Kefe05 <oghenekefe.okpare@miva.edu.ng>
2026-06-03 06:51:46 +0100

    intial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Page() {
+  return (
+    <div className="flex justify-center align-center min-h-svh p-6">
+      Home
+      
+    </div>
+  )
+}
